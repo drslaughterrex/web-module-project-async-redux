f53e11e9ad10377edb5f61a8883301bf2cc6d16c
Daniel McGraw <75826179+drslaughterrex@users.noreply.github.com>
2021-05-12 15:34:41 -0500

    MVP, no styling yet

---
+++ useless-knowledge-generator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Facts from './components/Facts';
+
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+     <h1>Learn a random, useless fact!</h1>
+    <Facts/>
+   </div>
+  );
+}
+
+export default App;
